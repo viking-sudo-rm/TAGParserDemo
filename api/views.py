34da902b08f9d0b51196f0366a8cf3a2296156ae
William Merrill <vikingarnir.will@gmail.com>
2018-03-30 01:02:50 -0400

    successful displaCy

--- api/views.py
+++ api/views.py
@@ -8,13 +8,9 @@ import json, os, sys, pickle, traceback
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.contrib.staticfiles.management.commands.runserver import Command as RunserverCommand
 
 PATH_TO_PARSER = "graph_parser"
-cwd = os.getcwd()
-os.chdir(PATH_TO_PARSER)
-
-sys.path.insert(0, os.path.abspath("."))
-import utils
 
 def read_sents(sents_file):
 	with open(sents_file) as fhand:
@@ -22,50 +18,40 @@ def read_sents(sents_file):
 
 def get_parse(test_opts):
 	outputs = []
-	sents = read_sents(test_opts.text_test)
-	stags = read_sents(test_opts.predicted_stags_file)
-	pos = read_sents(test_opts.predicted_pos_file)
-	arcs = read_sents(test_opts.predicted_arcs_file)
-	rels = read_sents(test_opts.predicted_rels_file)
-	with open(os.path.join(test_opts.base_dir, 'predicted_conllu', 'test.conllu'), 'wt') as fout:
-		for sent_idx in xrange(len(sents)):
-			output = ""
-			sent = sents[sent_idx]
-			stags_sent = stags[sent_idx]
-			pos_sent = pos[sent_idx]
-			arcs_sent = arcs[sent_idx]
-			rels_sent = rels[sent_idx]
-			print(len(sent), sent)
-			print(len(stags_sent), stags_sent)
-			for word_idx in xrange(len(sent)):
-				line = [str(word_idx+1)]
-				line.append(sent[word_idx])		##
-				line.append('_') 				##
-				line.append(sent[word_idx])
-				line.append('_')
-				line.append(stags_sent[word_idx])
-				line.append(pos_sent[word_idx])
-				line.append('_')
-				line.append('_')				##
-				line.append(arcs_sent[word_idx])
-				line.append('_')				##
-				line.append(rels_sent[word_idx])
-				line.append('_')
-				output += '\t'.join(line)
-				output += '\n'
-			outputs.append(output)
-	return outputs
+	sent = read_sents(test_opts.text_test)[0]
+	# FIXME: after the first sentence, parse output doesn't update!
+	stags_sent, arcs_sent, rels_sent = model.predict(session)
+	# arcs_sent = arcs[sent_idx]
+	# rels_sent = rels[sent_idx]
+	print(len(sent), sent)
+	print(len(stags_sent), stags_sent)
+	print(len(arcs_sent), arcs_sent)
+	print(len(rels_sent), rels_sent)
+	arcs = [{
+		"start": min(i, int(arcs_sent[i]) - 1), # int(arcs_sent[i]) - 1
+		"end": max(i, int(arcs_sent[i]) - 1), # i
+		"dir": "right" if i < int(arcs_sent[i]) - 1 else "left", # right
+		"label": rels_sent[i],
+	} for i in xrange(len(sent)) if rels_sent[i] != "ROOT"]
+	words = [{
+		"tag": stags_sent[i],
+		"text": sent[i],
+	} for i in xrange(len(sent))]
+	return {"arcs": arcs, "words": words}
 
-def output_sents(sents, test_opts):
-	sents = sent_tokenize(sents)
-	print(sents)
-	sents = map(word_tokenize, sents)
-	print(sents)
+def output_sents(text, test_opts):
+	sent = word_tokenize(sent_tokenize(text)[0])
+	# sents = map(word_tokenize, sents)
 	path = os.path.join(test_opts.base_dir, 'sents', 'test.txt')
-	with open(path, 'wt') as fout:
-		for sent in sents:
-			fout.write(' '.join(sent))
-			fout.write('\n')
+	with open(path, 'w') as fout:
+		# for sent in sents:
+		fout.write(' '.join(sent))
+		fout.write('\n')
+
+cwd = os.getcwd()
+os.chdir(PATH_TO_PARSER)
+sys.path.insert(0, os.path.abspath("."))
+import utils
 
 with open('demo/configs/config_demo.pkl') as fin:
 	opts = pickle.load(fin)
@@ -76,6 +62,7 @@ with open('demo/configs/config_demo_test.pkl') as fin:
 print("Loading saved parser session..")
 graph = tf.Graph()
 with graph.as_default():
+	print("Using {}.".format(opts.model))
 	Model = getattr(utils, opts.model)
 	model = Model(opts, test_opts)
 	session = tf.Session()
@@ -102,13 +89,14 @@ def parse(request):
 
 	os.chdir(PATH_TO_PARSER)
 	text = args["text"]
-	print(text, "|")
+
 	try:
 		output_sents(text, test_opts)
-		model.run_epoch(session, True)
+		# model.run_epoch(session, testmode=True)
+		# print(model.predict(session))
 		parse = get_parse(test_opts)
 		os.chdir(cwd)
-		return JsonResponse({"conllu": conllu}, safe=False)
+		return JsonResponse({"parse": parse}, safe=False)
 	except:
 		os.chdir(cwd)
 		print('-' * 60)
