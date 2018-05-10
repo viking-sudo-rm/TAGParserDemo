016628eb85e52ae6a2714592e9eba9ae4b2b6d27
William Merrill <vikingarnir.will@gmail.com>
2018-05-10 18:26:41 -0400

    STATIC_ROOT

--- TAGParserDemo/settings.py
+++ TAGParserDemo/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'demo/static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'demo/static')
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'demo/static'),
 )
