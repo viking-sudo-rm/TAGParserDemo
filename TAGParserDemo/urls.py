5a35cd22b099c7a3e55a3d589dbccec84841bd8e
William Merrill <vikingarnir.will@gmail.com>
2018-05-12 16:01:36 -0400

    moved to urls.py again

--- TAGParserDemo/urls.py
+++ TAGParserDemo/urls.py
@@ -25,3 +25,59 @@ urlpatterns = [
 # FIXME not ideal
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns += staticfiles_urlpatterns()
+
+#############################
+# Download all the files ONCE
+#############################
+
+import os
+from boto.s3.connection import S3Connection
+from boto.exception import S3ResponseError
+
+DATA_DIR = "s3"
+PARSER_DIR = "graph_parser"
+DEMO_DIR = os.path.join(DATA_DIR, "demo")
+GLOVE_DIR = os.path.join(DATA_DIR, "glovevector")
+MODEL_DIR = os.path.join(DEMO_DIR, "Pretrained_Parser/best_model")
+
+def download_bucket(bucket, path):
+
+    if not os.path.exists(path):
+        os.mkdir(path)
+    bucket_list = bucket.list()
+
+    for l in bucket_list:
+        key_string = str(l.key)
+        s3_path = os.path.join(path, key_string)
+        try:
+            print ("Current File is ", s3_path)
+            l.get_contents_to_filename(s3_path)
+        except (OSError, S3ResponseError) as e:
+            pass
+            # check if the file has been downloaded locally  
+            if not os.path.exists(s3_path):
+                try:
+                    os.makedirs(s3_path)
+                except OSError as exc:
+                    # let guard againts race conditions
+                    import errno
+                    if exc.errno != errno.EEXIST:
+                        raise
+
+BUCKET_NAME = os.environ["S3_BUCKET"]
+AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+AWS_ACCESS_SECRET_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+REGION = "s3.us-east-2.amazonaws.com"
+
+print("Bucket name", BUCKET_NAME)
+print("Access key", AWS_ACCESS_KEY_ID)
+print("Secret key", AWS_ACCESS_SECRET_KEY)
+print("Region", REGION)
+
+print("Connecting to AWS..")
+conn = S3Connection(AWS_ACCESS_KEY_ID, AWS_ACCESS_SECRET_KEY, host=REGION)
+print("Getting bucket..")
+bucket = conn.get_bucket(BUCKET_NAME)
+print("Accessing bucket..")
+download_bucket(bucket, DATA_DIR)
+print("Bucket downloaded successfully!")
