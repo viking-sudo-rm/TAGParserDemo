71675d9c53b8097d60e1edd2916152e8513ec604
William Merrill <vikingarnir.will@gmail.com>
2018-05-12 14:47:10 -0400

    converted to tabs

--- api/views.py
+++ api/views.py
@@ -18,22 +18,22 @@ def save_bucket(bucket, path):
 	bucket_list = bucket.list()
 
 	for l in bucket_list:
-        key_string = str(l.key)
-        s3_path = path + key_string
-        try:
-            print ("Current File is ", s3_path)
-            l.get_contents_to_filename(s3_path)
-        except (OSError,S3ResponseError) as e:
-            pass
-            # check if the file has been downloaded locally  
-            if not os.path.exists(s3_path):
-                try:
-                    os.makedirs(s3_path)
-                except OSError as exc:
-                    # let guard againts race conditions
-                    import errno
-                    if exc.errno != errno.EEXIST:
-                        raise
+		key_string = str(l.key)
+		s3_path = path + key_string
+		try:
+			print ("Current File is ", s3_path)
+			l.get_contents_to_filename(s3_path)
+		except (OSError,S3ResponseError) as e:
+			pass
+			# check if the file has been downloaded locally  
+			if not os.path.exists(s3_path):
+				try:
+					os.makedirs(s3_path)
+				except OSError as exc:
+					# let guard againts race conditions
+					import errno
+					if exc.errno != errno.EEXIST:
+						raise
 
 DATA_DIR = 's3'
 BUCKET_NAME = "tagparserdemo"
@@ -71,10 +71,10 @@ with graph.as_default():
 		saver.restore(session, MODEL_DIR)
 
 def word_tokenize_period(sent):
-    words = word_tokenize(sent)
-    if words[-1] not in ['.', '?']:
-        words.append('.')
-    return words
+	words = word_tokenize(sent)
+	if words[-1] not in ['.', '?']:
+		words.append('.')
+	return words
 
 def get_parse(sents, session=session):
 	# FIXME -- rn this assumes len(sents) == 1
